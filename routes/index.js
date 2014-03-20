a08fe2c5d52bed1742ad41dc0bd3fc75f3220633
Allen Zhong <allenzhong@Allens-iMac.local>
2014-03-20 21:43:14 +0800

    create menus

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,19 @@ exports.index = function(req, res) {
     }
 };
 
+exports.create_menu = function(req, res) {
+    getAccessToken(function(token) {
+        console.log(token);
+        createMenu(token, function(ress) {
+            var resJson = {
+                msg: ress.responseText,
+                token: token
+            };
+            res.send(resJson);
+        });
+    });
+};
+
 exports.doMessage = function(req, res) {
     //console.log(req.rawBody);
     //res.send(req.rawBody);
@@ -68,40 +81,63 @@ checkSignature = function(req) {
 var appID = "wxba50ad44bb9be2db";
 var appsecrect = "1e46b3602cd99f55154bcb8d1a8b1b25";
 var accessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s";
-//Get Access Token
+var createMenuUrl = " https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s";
+//Get Access Token,GET
 //callback(json)
+// json: {
+//     "access_token": "xxx", 
+//     "expires_in": 7200
+// }
 getAccessToken = function(callback) {
     var url = util.format(accessTokenUrl, appID, appsecrect);
     http.get(url, function(res) {
-        console.log(res.responseText);
-        callback(res.responseText);
+        console.log("after access token ->" + res.responseText);
+        var json = JSON.parse(res.responseText);
+        callback(json.access_token);
     });
 };
-// //Handle xml 
-// //callback(json)
-// readXml = function(xml, callback) {
-//     xmlreader.read(xml, function(err, res) {
-//         console.log("xmlreader.read");
-//         var json = {
-//             ToUserName: res.xml.ToUserName.text(),
-//             FromUserName: res.xml.FromUserName.text(),
-//             CreateTime: res.xml.CreateTime.text(),
-//             MsgType: res.xml.MsgType.text(),
-//             Content: res.xml.Content.text(),
-//             MsgId: res.xml.MsgId.text()
-//         };
-//         callback(json);
-//     });
-// };
 
-// buildXml = function(json, callback) {
-//     console.log("buildXml");
-//     var xml = xmlbuilder.create("xml");
-//     xml.ele("ToUserName").dat(json.FromUserName);
-//     xml.ele("FromUserName").dat(json.ToUserName);
-//     xml.ele("CreateTime").dat(json.CreateTime);
-//     xml.ele("MsgType").dat(json.MsgType);
-//     xml.ele("Content").dat(json.Content);
-//     //console.log(xml);
-//     callback(xml);
-// };
+var menuJson = {
+    button: [{
+        type: "click",
+        name: "今日歌曲",
+        key: "V1001_TODAY_MUSIC"
+    }, {
+        type: "click",
+        name: "歌手简介",
+        key: "V1001_TODAY_SINGER"
+    }, {
+        name: "菜单",
+        sub_button: [{
+            type: "view",
+            name: "搜索",
+            url: "http://www.soso.com/"
+        }, {
+            type: "view",
+            name: "视频",
+            url: "http://v.qq.com/"
+        }]
+    }]
+};
+//create menu,POST
+createMenu = function(token, callback) {
+    var url = util.format(createMenuUrl, token);
+    var jsonString = JSON.stringify(menuJson);
+    var headers = {
+        'Content-Type': 'application/json',
+        'Content-Length': jsonString.length
+    };
+    var options = {
+        host: "https://api.weixin.qq.com",
+        port: 80,
+        path: "cgi-bin/menu/create?access_token=" + token,
+        method: "POST",
+        headers: headers
+    };
+    var req = http.request(options, function(res) {
+        if (typeof(callback) == "function")
+            callback(res);
+    });
+    req.write(jsonString);
+    req.end();
+};
